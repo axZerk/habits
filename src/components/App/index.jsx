9ccfaa409e151cb7b039f7271a733dad2292b293
Alexander Repeta <axzerk@gmail.com>
2018-05-05 12:00:17 +0300

    adds authentication context tied to App state

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,17 +3,22 @@ import { Switch } from 'react-router-dom';
 import PrivateRoute from '../PrivateRoute';
 import AppBar from 'components/AppBar';
 import routerConfig from 'routes';
+import { AuthContext } from 'context';
 
 export default class App extends Component {
   state = {
-    isLoggedIn: false,
+    email: null,
+    displayName: null,
+    isLoading: false,
+    userId: null,
+    isAuthenticated: false,
   };
 
   render() {
-    const { isLoggedIn } = this.state;
+    const { isAuthenticated } = this.state;
 
     return (
-      <div>
+      <AuthContext.Provider value={{ ...this.state }}>
         <AppBar />
         <Switch>
           {routerConfig.map(route => (
@@ -21,12 +26,14 @@ export default class App extends Component {
               key={route.path}
               path={route.path}
               component={route.component}
-              isAuthenticated={route.protected ? isLoggedIn : !isLoggedIn}
+              isAuthenticated={
+                route.protected ? isAuthenticated : !isAuthenticated
+              }
               redirectTo={route.redirectPath}
             />
           ))}
         </Switch>
-      </div>
+      </AuthContext.Provider>
     );
   }
 }
--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Navigation from '../Navigation';
+import { AuthContext } from 'context';
 import styles from './styles.css';
 
 const AppBar = () => (
-  <header className={styles.header}>
-    <Navigation />
-  </header>
+  <AuthContext.Consumer>
+    {context => (
+      <header className={styles.header}>
+        <Navigation {...context} />
+      </header>
+    )}
+  </AuthContext.Consumer>
 );
 
 export default AppBar;
