f09867cef2fb99876da845cd78b7f50d6c508eba
Miniaylo.S <po4tovuj@gmail.com>
2018-05-07 10:50:22 +0300

    add RegistForm

--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import RegistForm from '../forms/RegisterForm';
+// import { auth } from "../../firebase/config";
 
 const RegisterPage = () => (
   <div>
     <h1>Register Page</h1>
+    <RegistForm onSubmit={(x)=> console.log(x)}/>
   </div>
 );
 
