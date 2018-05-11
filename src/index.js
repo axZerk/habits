63dca1f8944e4c063e15800e9cddadc08f152229
Alexander Repeta <axzerk@gmail.com>
2018-05-11 11:34:31 +0300

    adds hot reloading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(
-  <Router>
-    <Route path="/" component={App} />
-  </Router>,
-  document.getElementById('root'),
-);
+const rootEl = document.getElementById('root');
 
+const render = () => {
+  const App = require('./components/App').default;
+
+  ReactDOM.render(
+    <Router>
+      <Route path="/" component={App} />
+    </Router>,
+    rootEl,
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    setTimeout(render);
+  });
+}
+
+render();
 registerServiceWorker();
